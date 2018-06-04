4f2c0ffe256eeb65e2fe7e6761b0716aae49c982
Gokhan Cakal <gokhancakal.metu@gmail.com>
2018-06-05 02:22:22 +0300

    project is finalized.

--- Project 3/Wind_Turbine_Generator_Analytical_Design.m
+++ Project 3/Wind_Turbine_Generator_Analytical_Design.m
@@ -26,6 +26,12 @@ dia_strand = 1.45e-3;   %m, AWG 12 wire diameter
 ff = 0.5; %fill factor
 slot_mount = 2e-3;   %m, slot mount
 
+temp = 75;  %deg, operation temperature
+ro_cu_20 = 1.68e-8;      %ohm*m, resistivity of copper at 20 deg
+temp_ref = 20;    %deg, temp at ro_cu is defined
+temp_coef = 0.004041;   %temperature dependency of copper resistance
+
+
 q_s = 2;  %number of slots per pole per phase
 m = 3;  %phase number
 
@@ -57,7 +63,6 @@ l_eff = dia_bore * asp_ratio;  %m, effective axial length
 
 g_unrounded = 0.18e-3 + 0.006*(p_out)^(0.4)*1e-3; %m, air gap
 g = round(g_unrounded,3);
-g=3e-3;
 
 %% stator design
 
@@ -67,9 +72,9 @@ slot_pitch = pi * dia_in_stator / Q_s;  %m, stator slot pitch
 
 %% teeth width calculation
 
-% b_teeth_avg = b_teeth_peak * 2/pi;   %T, average teeth flux density
 l_act = l_eff - 2*g;   %m, actual axial length
 width_teeth = l_eff * slot_pitch * mag_load_peak / (stack * l_act * b_teeth_peak );
+
 %% winding factor 
 
 kp = 1; %pitch factor, single layer
@@ -109,28 +114,73 @@ depth_yoke = flux_pp / (2*stack*l_act*b_yoke_peak);   %m,depth of yoke
 dia_out_stator = dia_in_stator + 2*slot_mount + 2*(slot_depth+depth_yoke); %m
 dia_out_rotor = dia_bore-g; %m
 
-%% torque calculation
+%% effective air gap
 
-rotor_vol = pi*(dia_out_rotor/2)^2*l_eff; 
-stress = el_load_peak * mag_load_peak * pf_init/2;
-torque = 2*stress*rotor_vol;
-
-
+k = ( width_teeth / g ) / ( 5 + width_teeth / g ); 
+k_cs = slot_pitch / ( slot_pitch - k*width_teeth );  % carter's coefficient
+g_eff = k_cs * g;  %m, effective air gap ignoring rotor teeths
 
 
+%% magneting inductance 
+
+u0 = pi*4e-7;  %vacuum permeability
+Lm_ph = 2*u0 * dia_in_stator * l_eff * ( kw_1 * number_of_series_turns )^2 / ( pi * (p/2)^2 * g_eff );  %H, magnetizing inductance per phase
+Lm = Lm_ph * m/2;  %H, total magnetizing inductance
+
+%% resistance
+
+l_end_winding = ( dia_in_stator + slot_depth ) * pi * 1.1 / p; %m, end winding length
+slots_per_phase = Q_s / m;  % slots per phase
+l_total_per_turn = 2*( l_end_winding + l_act );  %m, total length per turn 
+
+ro_cu = ro_cu_20 * (1+temp_coef*(temp-temp_ref));     %ohm*m, resistivity of copper at operating temperature
+
+res_per_strand_per_turn = ro_cu * l_total_per_turn / area_strand;  %ohm, resistance per strand per turn
+
+res_per_cond_per_turn = res_per_strand_per_turn / number_of_strands_per_cond;  %ohm
+
+res_ph = number_of_series_turns * res_per_cond_per_turn; %ohm, phase resistance
 
 
+%% winding factors for harmonics
+
+coil_span = pi;  %rad, full pitch coils
+
+for h = 1:1:15
+    
+    kp(h) = sin(h*coil_span/2);  %pitch factor is equal to one due to full pitch
+    kd(h) = sin(h*q_s*slot_ang/2) / (q_s*sin(h*slot_ang/2));  %dist factor
+    kw(h) = kp(h) * kd(h) * mod(h,2);    %winding factor
+end
 
 
+%% efficiency calculation
+
+p_cu_loss_stator = cur_stator^2 * res_ph * 3;  %W, stator copper losses
+p_loss_fw = 2e3; %W, friction and windage losses
+p_loss_stray = 2.5e3;  %W, stray losses
+p_loss_core = 1e3;  %W, iron core losses
+p_loss_rotor = 3e3;  %W, rotor ohmic losses
+p_loss = p_cu_loss_stator+p_loss_fw+p_loss_stray+p_loss_core+p_loss_rotor;  %W, total losses
+
+eff = p_out / (p_out+p_loss)*100;  %efficiency
 
 
+%% mass calculation
 
+dens_cu = 8900;   %kg/m3, copper density
+dens_core = 7650;     %kg/m3, core density
+dia_in_rotor = 100e-3;  %m, rotor inner diameter
 
+vol_stator = pi * l_act * stack * ( (dia_out_stator/2)^2 - (dia_in_stator/2+20e-3)^2 );  %m3, stator volume
+vol_rotor = pi * l_act * stack * ( (dia_out_rotor/2)^2 - (dia_in_rotor/2)^2 );  %m3, stator volume
+vol_cu = area_strand * l_total_per_turn * number_of_strands_per_cond * number_of_series_turns * m;  %m3, copper volume
 
+mass_stator = dens_core * vol_stator;  %kg, stator mass
+mass_rotor = dens_core * vol_rotor;   %kg, rotor mass
+mass_cu = dens_cu * vol_cu;   %kg, copper mass
 
-
-
-
+mass_total = mass_stator + mass_rotor + mass_cu;  %kg, total motor mass
 
 
 
